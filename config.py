963a31ce1fef0576d88df981dff0f9fb60d6f94b
Umar565 <titaniumgamingr@gmail.com>
2024-11-24 22:11:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,10 +5,10 @@ from os import getenv
 
 API_ID = int(getenv("API_ID", "9972455"))
 API_HASH = getenv("API_HASH", "5b1fd83b698e4e6670f3dcb053eecc06")
-BOT_TOKEN = getenv("BOT_TOKEN", "7212745746:AAF0ocuS_1eFzwfhkjr4WZRv_tzRTqlGME0")
+BOT_TOKEN = getenv("BOT_TOKEN", "7430047499:AAF5Et-Y2Zm1lAAp9hVM-_T3X8u0Vu5XslA")
 OWNER_ID = list(map(int, getenv("OWNER_ID", "6928637808").split()))
-MONGO_DB = getenv("MONGO_DB", "mongodb+srv://Restrictedbotforeveryone:publicontentsaver@restrictedbot.yah0l.mongodb.net/?retryWrites=true&w=majority&appName=Restrictedbot")
+MONGO_DB = getenv("MONGO_DB", "mongodb+srv://mainmusicbot:musicbotdatabase@cluster0.hbvhe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 LOG_GROUP = getenv("LOG_GROUP", "-1002357764876")
-CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002234404301"))
+CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002391820880"))
 FREEMIUM_LIMIT = int(getenv("FREEMIUM_LIMIT", "0"))
 PREMIUM_LIMIT = int(getenv("PREMIUM_LIMIT", "1000"))
