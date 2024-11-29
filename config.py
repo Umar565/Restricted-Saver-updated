8accbdfe45e4b32b1bfbb0cac672cdd62c6d5a35
Umar565 <titaniumgamingr@gmail.com>
2024-11-29 15:00:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ from os import getenv
 
 API_ID = int(getenv("API_ID", "9972455"))
 API_HASH = getenv("API_HASH", "5b1fd83b698e4e6670f3dcb053eecc06")
-BOT_TOKEN = getenv("BOT_TOKEN", "7692845942:AAFkMwOAikNzzwOV-kluIMNor0bg597GsIs")
+BOT_TOKEN = getenv("BOT_TOKEN", "7692845942:AAHoMsMySG3WxVsidVXRXXt4f1I9GCfzwyo")
 OWNER_ID = list(map(int, getenv("OWNER_ID", "6928637808").split()))
 MONGO_DB = getenv("MONGO_DB", "mongodb+srv://vpsrestrictedsaver:AWSrestrictedsaver@cluster0.a3ejs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 LOG_GROUP = getenv("LOG_GROUP", "-1002418022572")
